0fb94b3b05a5979139b8d821608d837308367dac
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2022-06-22 12:33:08 +0530

    Switching to the functional API

--- not_naive.py
+++ not_naive.py
@@ -28,10 +28,25 @@ model.summary()
 
 # EXTRACTION PHASE
 data = tdfs.load("malaria", split='train', as_supervised=True)
+
+file_pattern = f'~/tensorflow_datasets/malaria/1.0.0/malaria-train.tfrecord*'
+file = tf.data.Dataset.list_files(file_pattern)
+
+train_dataset = file.interleave(
+    tf.data.TFRecordDataset,
+    cycle_length=4,
+    num_parallel_calls=tf.data.experimental.AUTOTUNE
+)
 # EXTRACTION PHASE END
 
 # TRANSFORMATION PHASE
-def augmentation(image, label):
+def augmentationV2(serialized_data):
+    description ={
+        'image': tf.io.FixedLenFeature((), tf.string),
+        'label': tf.io.FixedLenFeature((), tf.int64, -1)
+    }
+    example = tf.io.parse_single_example(serialized_data, description)
+    image = tf.image.decode_jpeg(example['image'], channels=3)
     image = tf.cast(image, tf.float32)
     image = tf.image.resize(image, (100, 100))
     image = image / 255
@@ -39,15 +54,22 @@ def augmentation(image, label):
     image = tfa.image.rotate(image, 40, interpolation='NEAREST')
     image = tf.image.random_brightness(image, 0.1)
     image = tf.image.random_contrast(image, 0.8, 1.2)
-    return image, label
+    return image, example['label']
 
-train = data.map(augmentation)
-train_batches = train.shuffle(100).batch(32)
-# TRANSFORMATION PHASE END
+import multiprocessing
+cores = multiprocessing.cpu_count()
+
+train_dataset = train_dataset.map(augmentationV2, num_parallel_calls=cores)
+train_dataset = train_dataset.cache()
+
+train_dataset = train_dataset.shuffle(100).batch(32)
+train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+# TRANSROFMATION PHASE END
+
 
 # LOAD PHASE
 history = model.fit(
-    train_batches,
+    train_dataset,
     epochs=15
 )
 # LOAD PHASE END
